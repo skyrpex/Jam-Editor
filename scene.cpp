7e216790004ade9723986e91f69f40bdd005c728
Skyrpex <skyrpex@gmail.com>
2014-01-23 22:32:49 +0100

    Cleanup scene

--- scene.cpp
+++ scene.cpp
@@ -7,19 +7,18 @@ const QColor GridColor{230, 230, 230};
 
 void Scene::drawBackground(QPainter *painter, const QRectF &rect)
 {
-    const QRectF target = sceneRect().intersected(rect);
-
+    // Draw grid
     {
         const int gridSize = 25;
-        qreal left = int(target.left()) - (int(target.left()) % gridSize);
-        qreal top = int(target.top()) - (int(target.top()) % gridSize);
+        qreal left = int(rect.left()) - (int(rect.left()) % gridSize);
+        qreal top = int(rect.top()) - (int(rect.top()) % gridSize);
 
         QVarLengthArray<QLineF, 100> lines;
 
-        for (qreal x = left; x < target.right(); x += gridSize)
-            lines.append(QLineF(x, target.top(), x, target.bottom()));
-        for (qreal y = top; y < target.bottom(); y += gridSize)
-            lines.append(QLineF(target.left(), y, target.right(), y));
+        for (qreal x = left; x < rect.right(); x += gridSize)
+            lines.append(QLineF(x, rect.top(), x, rect.bottom()));
+        for (qreal y = top; y < rect.bottom(); y += gridSize)
+            lines.append(QLineF(rect.left(), y, rect.right(), y));
 
         //    qDebug() << lines.size();
 
@@ -27,7 +26,8 @@ void Scene::drawBackground(QPainter *painter, const QRectF &rect)
         painter->drawLines(lines.data(), lines.size());
     }
 
+    // Draw origin
     painter->setPen(OriginColor);
-    painter->drawLine(target.left(), 0, target.right(), 0);
-    painter->drawLine(0, target.top(), 0, target.bottom());
+    painter->drawLine(rect.left(), 0, rect.right(), 0);
+    painter->drawLine(0, rect.top(), 0, rect.bottom());
 }
